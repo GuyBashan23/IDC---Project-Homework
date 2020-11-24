0bd51806f84e29fce7a82924a7501f91ac431773
tagbox-mini <guybashan@outlook.com>
2020-11-24 18:55:41 +0200

    is NOT a prime number!

--- ex4_206266991.py
+++ ex4_206266991.py
@@ -3,7 +3,7 @@ n = 7
 if n > 1:
    for i in range(2,n):
        if (n % i) == 0:
-           print(n,"is NOT a prime number")
+           print(n,"is NOT a prime number!")
            break
    else:
        print(n,"is a prime number")
